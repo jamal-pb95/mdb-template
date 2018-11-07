00772d2fe3f3db95eaf6b9d36b9b10fcf04642aa
jamal-pb95 <jamal.pb95@gmail.com>
2018-11-07 17:02:55 +0600

    app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { random } from 'lodash';
+import axios from 'axios';
+import './App.css'
+import 'typeface-roboto';
+import { Grid, withStyles } from '@material-ui/core';
+import QuoteGenerator from './components/QuoteGenerator';
 
+// quote json file
+const URL = 'https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json';
+
+// main styles
+const styles = {
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    height: '100vh'
+  }
+};
+
+
+// App begin
 class App extends Component {
+  state = {
+    quotes: [],
+    selectedQuoteIndex: null
+  };
+
+  // fetch data from url json file
+  componentDidMount() {
+    axios.get(URL)
+      .then((res) => res.data)
+      .catch(error => console.log(error))
+      .then(quotes => this.setState({ quotes }, this.assignNewQuoteIndex));
+  }
+
+  // check the index of quote array
+  get selectedQuote() {
+    if(!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
+      return undefined;
+    }
+    return this.state.quotes[this.state.selectedQuoteIndex];
+  }
+
+  // select random index
+  selectedQuoteIndex = () => {
+    if(!this.state.quotes.length) return undefined;
+    return random(0, this.state.quotes.length - 1);
+  }
+
+  // assign random index
+  assignNewQuoteIndex = () => {
+    this.setState({
+      selectedQuoteIndex: this.selectedQuoteIndex()
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grid className={this.props.classes.container} id="quote-box" justify="center" container>
+        <Grid xs={10} lg={8} color="primary" item>
+          {
+            /*
+            * first check if selectedQuote exist of not?
+            * if have then show the quote and the author
+            * otherwise show null 
+            */
+          }
+          {
+            this.selectedQuote ?
+            <QuoteGenerator 
+              selectedQuote={this.selectedQuote}
+              assignNewQuoteIndex={this.assignNewQuoteIndex} /> :
+            null
+          }
+        </Grid>
+      </Grid>
     );
   }
 }
 
-export default App;
+// Pass the App with styles
+export default withStyles(styles)(App);
